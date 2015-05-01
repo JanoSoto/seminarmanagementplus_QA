355af9e7976bafd86cceda309e936f2e25d8b107
Celso Gutiérrez <celso.gutierrez@usach.cl>
2015-05-01 01:41:30 -0300

    pdf plantilla tema

---
+++ SeminarManagement-Maven-web/src/main/java/managedbeans2/temas/PlantillaTema.java
@@ -0,0 +1,235 @@
+package managedbeans2.temas;
+
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfStamper;
+import entities.Alumno;
+import entities.ProfeCorreccion;
+import entities.ProfePropuesta;
+import entities.Profesor;
+import entities.Tema;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author stateless
+ */
+@WebServlet(name = "PlantillaTema", urlPatterns = {"/PdfTema"})
+public class PlantillaTema extends HttpServlet {
+
+    public static final String TEMPLATE_LOCATION = "/resources/plantillas/plantilla_tema.pdf";
+    
+    @Inject
+    VerTemaMB temaMB;
+    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        ByteArrayOutputStream baosPDF = null;
+        
+        try {
+            if (temaMB.getIdTema() == null){
+                if (request.getParameter("tema") != null){
+                    temaMB.setIdTema(Integer.parseInt(request.getParameter("tema")));
+                    temaMB.buscarTema();
+                } else {
+                    throw new NullPointerException("No se especificó un tema");
+                }
+            }
+
+            Tema tema = temaMB.getTema();
+            Alumno alumno = tema.getIdRevisora().getIdPropuesta().getRutAlumno();
+            
+            InputStream resourceUrl = getServletContext().getResourceAsStream(TEMPLATE_LOCATION);
+
+            baosPDF = new ByteArrayOutputStream();
+            PdfReader pdfTemplate = new PdfReader(resourceUrl);
+            PdfStamper stamper = new PdfStamper(pdfTemplate, baosPDF);
+            stamper.setFormFlattening(true);
+            
+            StringBuilder str = new StringBuilder();
+            str.append("INFORME DE REVISION DE MEMORIA PARA OPTAR AL TITULO  DE INGENIERO ");
+            str.append( alumno.getCarreraAlumno() == 0 ?
+                    "CIVIL EN INFORMATICA - " :
+                    "DE EJECUCION EN COMPUTACION E INFORMATICA - ");
+            str.append(tema.getIdSemestre().getIdSemestre().replace("/", "° "));
+            
+            stamper.getAcroFields().setField("doc_title", str.toString());
+            stamper.getAcroFields().setField("title", tema.getNombreTema());
+            
+            str = new StringBuilder();
+            str.append(alumno.getNombreAlumno()).append(" ").append(alumno.getApellidoAlumno());
+            stamper.getAcroFields().setField("student_name", str.toString());
+            
+            str = new StringBuilder("");
+            for (ProfePropuesta profe : tema.getIdRevisora().getIdPropuesta().getProfePropuestaList() ) {
+                if (profe.getRolGuia() == 1){
+                    str.append(profe.getProfesor().getNombreProfesor()).append(" ")
+                            .append(profe.getProfesor().getApellidoProfesor());
+                    break;
+                }
+            }
+            stamper.getAcroFields().setField("guide_proffesor", str.toString());
+            
+            str = new StringBuilder("");
+            for (ProfePropuesta profe : tema.getIdRevisora().getIdPropuesta().getProfePropuestaList() ) {
+                if (profe.getRolGuia() == 0){
+                    str.append(profe.getProfesor().getNombreProfesor()).append(" ")
+                            .append(profe.getProfesor().getApellidoProfesor());
+                    break;
+                }
+            }
+            stamper.getAcroFields().setField("co_guide_proffesor", str.toString());
+            
+            Profesor corrector1 = null, corrector2 = null;
+            if( tema.getIdCorrectora() != null ){
+                for( ProfeCorreccion pc : tema.getIdCorrectora().getProfeCorreccionList()){
+                    if( pc.getRolCorreccion()== 0 )
+                        corrector1 = pc.getProfesor();
+                    if( pc.getRolCorreccion()== 1 )
+                        corrector2 = pc.getProfesor();
+                }
+            }
+
+            if (corrector1 != null){
+                str = new StringBuilder(corrector1.getNombreProfesor());
+                str.append(" ").append(corrector1.getApellidoProfesor());
+                stamper.getAcroFields().setField("commission_proffesor_1", str.toString());
+            }
+            
+            if ( corrector2 != null ){
+                str = new StringBuilder(corrector2.getNombreProfesor());
+                str.append(" ").append(corrector2.getApellidoProfesor());
+                stamper.getAcroFields().setField("commission_proffesor_2", str.toString());
+            }
+            
+            //formatear el rut
+            str = new StringBuilder(alumno.getRutAlumno());
+            if (str.length() > 1){
+                str.insert(str.length()-1, "-");
+                if (str.length() > 5){
+                    str.insert(str.length()-5, ".");
+                    if (str.length() > 9){
+                        str.insert(str.length()-9, ".");
+                    }
+                }
+            }
+            
+            stamper.getAcroFields().setField("student_rut", str.toString());
+            stamper.getAcroFields().setField("student_phone", alumno.getTelefonoAlumno());
+            stamper.getAcroFields().setField("student_email", alumno.getMailAlumno());
+            stamper.getAcroFields().setField("student_address", alumno.getDireccionAlumno());
+            
+            SimpleDateFormat formateador = new SimpleDateFormat("dd 'de' MMMM 'de' yyyy", new Locale("ES"));
+            str = new StringBuilder("Santiago, ");
+            Date date = new Date();
+            str.append(formateador.format(date));
+            stamper.getAcroFields().setField("date", str.toString());
+            
+            stamper.close();
+            pdfTemplate.close();
+
+            // Read the HTTP 1.1 specification for full details
+            // about the Cache-Control header
+            response.setHeader("Cache-Control", "max-age=30");
+            response.setContentType("application/pdf");
+            // The Content-disposition header is explained
+            // in RFC 2183
+            //    http://www.ietf.org/rfc/rfc2183.txt
+            // The Content-disposition value will be in one of 
+            // two forms:
+            //   1)  inline; filename=foobar.pdf
+            //   2)  attachment; filename=foobar.pdf
+            // In this servlet, we use "inline"
+
+            StringBuilder sbContentDispValue = new StringBuilder();
+            sbContentDispValue.append("inline")
+                .append("; filename=")
+                .append("Propuesta_")
+                .append(alumno.getRutAlumno())
+                .append(".pdf");
+
+            response.setHeader("Content-disposition", sbContentDispValue.toString());
+            response.setContentLength(baosPDF.size());
+            ServletOutputStream sos;
+            sos = response.getOutputStream();
+            baosPDF.writeTo(sos);
+            sos.flush();
+            
+        } catch (NumberFormatException | NullPointerException | DocumentException | IOException dex) {
+            System.out.println(dex);
+            //LOGGER.error(dex);
+            response.setContentType("text/html");
+            PrintWriter writer = response.getWriter();
+            writer.println("Documento no disponible<br>");
+            writer.println("<pre>");
+            writer.println("</pre>");
+        } finally {
+            if (baosPDF != null) {
+                baosPDF.reset();
+            }
+        }
+    }
+    
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
