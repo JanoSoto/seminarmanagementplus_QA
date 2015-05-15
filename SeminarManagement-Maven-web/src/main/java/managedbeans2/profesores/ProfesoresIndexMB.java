3815f67ec3ea2e1bb5e985d45eed37079917512a
Giovanni Benussi <giovanni.benussi@usach.cl>
2015-05-15 00:33:46 -0300

    Muestra temas vigentes por profesor en la sección estadísticas

--- SeminarManagement-Maven-web/src/main/java/managedbeans2/profesores/ProfesoresIndexMB.java
+++ SeminarManagement-Maven-web/src/main/java/managedbeans2/profesores/ProfesoresIndexMB.java
@@ -23,6 +23,8 @@ import javax.faces.context.FacesContext;
 import sessionbeans.ProfesorFacadeLocal;
 import sessionbeans.SemestreActualFacadeLocal;
 import Util.Util;
+import entities.Alumno;
+import sessionbeans.AlumnoFacade;
 
 /**
  *
@@ -30,89 +32,110 @@ import Util.Util;
  */
 @Named(value = "profesoresIndexMB")
 @RequestScoped
-public class ProfesoresIndexMB{
+public class ProfesoresIndexMB {
+
     @EJB
     private SemestreActualFacadeLocal semestreActualFacade;
     @EJB
     private ProfesorFacadeLocal profesorFacade;
-    
+
     private List<Profesor> profesores;
     private List<ProfeDatos2> profeDatos, profesoresFiltrados;
-    
+
     /**
      * Creates a new instance of ProfesoresIndexMB
      */
     public ProfesoresIndexMB() {
     }
-    
+
     @PostConstruct
     public void init() {
         String semestreActual;
         FacesContext context = FacesContext.getCurrentInstance();
         //Seteamos el semestre a semestre actual
-        if(semestreActualFacade.findAll().isEmpty()){
+        if (semestreActualFacade.findAll().isEmpty()) {
             semestreActual = "";
-            context.addMessage(null, new FacesMessage("Aviso","No se ha ingresado semestre actual en el sistema"));
-        }
-        else
+            context.addMessage(null, new FacesMessage("Aviso", "No se ha ingresado semestre actual en el sistema"));
+        } else {
             semestreActual = semestreActualFacade.findAll().get(0).getSemestreActual();
-        
+        }
+
         profesores = profesorFacade.findAll();
-        int guiaTemp,revisorTemp,guiaP, revProp, revSem;
+        int guiaTemp, revisorTemp, guiaP, revProp, revSem, guiaTempDiurno, guiaTempVespertino;
         ProfeDatos2 profeDatosTemp;
         profeDatos = new ArrayList();
         List<ProfeRevision> profRev;
-        for(int i=0;i<profesores.size();i++){
+        for (int i = 0; i < profesores.size(); i++) {
             profeDatosTemp = new ProfeDatos2();
-            profRev=profesores.get(i).getProfeRevisionList();
-            guiaTemp=0;
-            guiaP=0;
+            profRev = profesores.get(i).getProfeRevisionList();
+            guiaTemp = 0;
+            guiaTempDiurno = 0;
+            guiaTempVespertino = 0;
+            guiaP = 0;
             //Seteamos revisiones
-            revisorTemp=0;
-            revProp=0;
-            revSem=0;
-            for(int j=0;j<profRev.size();j++)
-                if(profRev.get(j).getComisionRevisora().getIdSemestre().getIdSemestre().equals(semestreActual)){
+            revisorTemp = 0;
+            revProp = 0;
+            revSem = 0;
+            for (int j = 0; j < profRev.size(); j++) {
+                if (profRev.get(j).getComisionRevisora().getIdSemestre().getIdSemestre().equals(semestreActual)) {
                     revisorTemp++;
-                    if(profRev.get(j).getComisionRevisora().getTipoRevision()==0)
+                    if (profRev.get(j).getComisionRevisora().getTipoRevision() == 0) {
                         revProp++;
-                    if(profRev.get(j).getComisionRevisora().getTipoRevision()==1)
+                    }
+                    if (profRev.get(j).getComisionRevisora().getTipoRevision() == 1) {
                         revSem++;
+                    }
                 }
+            }
             //Seteamos la cantidad de temas vigentes en que es guía
-            for(int j=0; j<profesores.get(i).getProfePropuestaList().size(); j++){
-                if(profesores.get(i).getProfePropuestaList().get(j).getRolGuia() == 0){
+            for (int j = 0; j < profesores.get(i).getProfePropuestaList().size(); j++) {
+                if (profesores.get(i).getProfePropuestaList().get(j).getRolGuia() == 0) {
                     guiaP++;
-                    if(profesores.get(i).getProfePropuestaList().get(j).getPropuesta() != null)
-                        if(profesores.get(i).getProfePropuestaList().get(j).getPropuesta().getIdRevisora() != null)
-                            if(profesores.get(i).getProfePropuestaList().get(j).getPropuesta().getIdRevisora().getIdTema() != null)
-                                if(profesores.get(i).getProfePropuestaList().get(j).getPropuesta().getIdRevisora().getIdTema().getEstadoTema() != null){
+                    if (profesores.get(i).getProfePropuestaList().get(j).getPropuesta() != null) {
+                        if (profesores.get(i).getProfePropuestaList().get(j).getPropuesta().getIdRevisora() != null) {
+                            if (profesores.get(i).getProfePropuestaList().get(j).getPropuesta().getIdRevisora().getIdTema() != null) {
+                                if (profesores.get(i).getProfePropuestaList().get(j).getPropuesta().getIdRevisora().getIdTema().getEstadoTema() != null) {
                                     int tipoTemaTemp = profesores.get(i).getProfePropuestaList().get(j).getPropuesta().getIdRevisora().getIdTema().getEstadoTema();
-                                    if(tipoTemaTemp == 0 || tipoTemaTemp == 2)
+                                    if (tipoTemaTemp == 0 || tipoTemaTemp == 2) {
                                         guiaTemp++;
+                                    }
+//                                    Alumno alumno = profesores.get(i).getProfePropuestaList().get(j).getPropuesta().getRutAlumno();
+//                                    if (alumno.getJornada() == 1) {
+//                                        guiaTempDiurno++;
+//                                    } else {
+//                                        guiaTempVespertino++;
+//                                    }
                                 }
+                            }
+                        }
+                    }
                 }
             }
             //Seteamos correcciones
             int corrTemp;
             List<Profesor> profes = profesorFacade.findAll();
             List<ProfeCorreccion> profCorr;
-            profCorr=profes.get(i).getProfeCorreccionList();
-            corrTemp=0;
-            for(int j=0;j<profCorr.size();j++)
-                if(profCorr.get(j).getComisionCorrectora().getIdSemestre().getIdSemestre().equals(semestreActual))
+            profCorr = profes.get(i).getProfeCorreccionList();
+            corrTemp = 0;
+            for (int j = 0; j < profCorr.size(); j++) {
+                if (profCorr.get(j).getComisionCorrectora().getIdSemestre().getIdSemestre().equals(semestreActual)) {
                     corrTemp++;
+                }
+            }
             profeDatosTemp.setCorrSemActual(corrTemp);
             profeDatosTemp.setProfesor(profesores.get(i));
             String rutP = profesores.get(i).getRutProfesor();
             profeDatosTemp.setRutFormateado(Util.formatearRut(rutP));
-            profeDatosTemp.setRutProfConGuion(rutP.substring(0, rutP.length()-1)+"-"+rutP.substring(rutP.length()-1));
-            if(profesores.get(i).getContrato() == 0)
+            profeDatosTemp.setRutProfConGuion(rutP.substring(0, rutP.length() - 1) + "-" + rutP.substring(rutP.length() - 1));
+            if (profesores.get(i).getContrato() == 0) {
                 profeDatosTemp.setContrato("Por Hora");
-            else
+            } else {
                 profeDatosTemp.setContrato("Jornada Completa");
+            }
             profeDatosTemp.setRevSemActual(revProp);
             profeDatosTemp.setTemasVigentes(guiaTemp);
+            profeDatosTemp.setTemasVigentesDiurno(guiaTempDiurno);
+            profeDatosTemp.setTemasVigentesVespertino(guiaTempVespertino);
             profeDatosTemp.setPropSemActual(guiaP);
             profeDatos.add(profeDatosTemp);
         }
@@ -140,5 +163,5 @@ public class ProfesoresIndexMB{
 
     public void setProfesores(List<Profesor> profesores) {
         this.profesores = profesores;
-    }    
+    }
 }
