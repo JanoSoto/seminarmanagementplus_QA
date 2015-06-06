b0003362db0045e57ead649d3122d230af854b0a
Celso Gutiérrez <celso.gutierrez@usach.cl>
2015-06-05 23:32:57 -0300

    avance us 49

--- SeminarManagement-Maven-ejb/src/main/java/sessionbeans/ComisionRevisoraFacadeLocal.java
+++ SeminarManagement-Maven-ejb/src/main/java/sessionbeans/ComisionRevisoraFacadeLocal.java
@@ -1,13 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package sessionbeans;
 
 import entities.ComisionRevisora;
-import entities.Tema;
 import java.util.List;
 import javax.ejb.Local;
 
