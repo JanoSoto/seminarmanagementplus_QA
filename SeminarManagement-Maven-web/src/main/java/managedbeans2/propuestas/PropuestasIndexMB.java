401b0b47bae86553dfbe0bc56981c8d1b3d51423
Celso Gutiérrez <celso.gutierrez@usach.cl>
2015-06-08 06:33:05 -0300

    avance reportes :/ nada aun

--- SeminarManagement-Maven-web/src/main/java/managedbeans2/propuestas/PropuestasIndexMB.java
+++ SeminarManagement-Maven-web/src/main/java/managedbeans2/propuestas/PropuestasIndexMB.java
@@ -1,17 +1,10 @@
 package managedbeans2.propuestas;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import clases.PropuestaDatos;
 import entities.ProfePropuesta;
 import entities.Propuesta;
 import entities.Semestre;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
