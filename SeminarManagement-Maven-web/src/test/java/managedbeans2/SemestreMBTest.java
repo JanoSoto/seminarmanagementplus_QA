09afc3544f5016483ef0a6420b0ccb315c4f19ef
SmokeInBrain <miguel.carcamo@usach.cl>
2015-06-08 23:17:29 -0300

    US de precierre lista

--- SeminarManagement-Maven-web/src/test/java/managedbeans2/SemestreMBTest.java
+++ SeminarManagement-Maven-web/src/test/java/managedbeans2/SemestreMBTest.java
@@ -128,4 +128,81 @@ public class SemestreMBTest {
         assertEquals(temaTest, temas);
     }
     
+    @Test
+    public void testPreTerminarSemestreActual() {
+        Semestre sm = new Semestre();
+        sm.setComisionCorrectoraList(null);
+        sm.setComisionRevisoraList(null);
+        sm.setIdSemestre("1");
+        sm.setPropuestaList(null);
+        
+        //Se crea un tema 1
+        Tema tm = new Tema();
+        tm.setComisionCorrectoraList(null);
+        tm.setComisionRevisoraList(null);
+        tm.setEstadoTema(0);
+        tm.setFechaTema("fecha");
+        tm.setIdCorrectora(null);
+        tm.setIdRevisora(null);
+        tm.setIdSemestre(sm);
+        tm.setIdTema(2);
+        tm.setNombreTema("nombretema");
+        
+        //Se crea tema 2
+        Tema tm2 = new Tema();
+        tm2.setComisionCorrectoraList(null);
+        tm2.setComisionRevisoraList(null);
+        tm2.setEstadoTema(2);
+        tm2.setFechaTema("fecha2");
+        tm2.setIdCorrectora(null);
+        tm2.setIdSemestre(sm);
+        tm2.setIdTema(2);
+        tm2.setNombreTema("nombretema2");
+        
+        //Se crea tema 3
+        Tema tm3 = new Tema();
+        tm3.setComisionCorrectoraList(null);
+        tm3.setComisionRevisoraList(null);
+        tm3.setEstadoTema(6);
+        tm3.setPrecerrado(false);
+        tm3.setFechaTema("fecha3");
+        tm3.setIdCorrectora(null);
+        tm3.setIdSemestre(sm);
+        tm3.setIdTema(3);
+        tm3.setNombreTema("nombretema3");        
+        List<Tema> temas = new ArrayList<>();
+        temas.add(tm);
+        temas.add(tm2);
+        temas.add(tm3);
+        
+        sm.setTemaList(temas);
+        
+        
+        
+        // preterminar semestre
+        
+        for (int i = 0; i < temas.size(); i++) {
+            if (temas.get(i).getEstadoTema() == 6){
+                temas.get(i).setPrecerrado(true);
+            }
+        }
+        
+        List<Tema> temaTest = new ArrayList<>();
+        tm.setEstadoTema(3);
+        tm2.setEstadoTema(3);
+        temaTest.add(tm);
+        temaTest.add(tm2);
+        temaTest.add(tm3);
+                
+        temaTest.get(0).getEstadoTema();
+        
+        when(entityManager.createNamedQuery("SELECT c FROM Tema c")).thenReturn(query);
+        
+        Query querys = entityManager.createNamedQuery("SELECT c FROM Tema c");
+        
+        when(querys.getResultList()).thenReturn(temaTest);
+
+        assertEquals(temaTest, temas);
+    }
+    
 }
