a230d1ccbcb67a11e34eff979f6746e7e8532b83
SmokeInBrain <miguel.carcamo@usach.cl>
2015-08-11 23:36:18 -0300

    US casi lista, falta revisar + arreglo de nombres de temas

--- SeminarManagement-Maven-web/src/main/webapp/resources/js/agregarComisionRevisora.js
+++ SeminarManagement-Maven-web/src/main/webapp/resources/js/agregarComisionRevisora.js
@@ -31,21 +31,29 @@ function showPanel(){
     }
     
     var $element2 = $("select[name='formulario:tipo2_input'] option:selected").val();
-    if ( $element2 === '2' ){ // consejo
+    if($element2 === '3'){
+        $( '#formulario\\:panelConsejo' ).hide();
+        $( '#formulario\\:panelSeminario' ).hide();
+        $( '#formulario\\:panelSecretaria' ).hide();
+        $( '#formulario\\:panelMagister' ).show();       
+    } else if ( $element2 === '2' ){ // consejo
         //console.log("2");
         $( '#formulario\\:panelConsejo' ).show();
         $( '#formulario\\:panelSeminario' ).hide();
         $( '#formulario\\:panelSecretaria' ).hide();
+        $( '#formulario\\:panelMagister' ).hide(); 
     } else if ( $element2 === '1') { //seminario
         //console.log("1");
         $( '#formulario\\:panelConsejo' ).hide();
         $( '#formulario\\:panelSeminario' ).show();
         $( '#formulario\\:panelSecretaria' ).hide();
+        $( '#formulario\\:panelMagister' ).hide(); 
     } else if ( $element2 === '0' ) { //secretaria
         //console.log("0");
         $( '#formulario\\:panelConsejo' ).hide();
         $( '#formulario\\:panelSeminario' ).hide();
         $( '#formulario\\:panelSecretaria' ).show();
+        $( '#formulario\\:panelMagister' ).hide(); 
     } else {
         //console.log("nada");
 
